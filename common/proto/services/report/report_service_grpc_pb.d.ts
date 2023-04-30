0183230aa1aa0e06628513a7de5628f3c6a352c0
Felipe Araujo <fbma@cin.ufpe.br>
2023-04-30 15:33:01 -0300

    Add listAllReports on reportGRPCService

--- common/proto/services/report/report_service_grpc_pb.d.ts
+++ common/proto/services/report/report_service_grpc_pb.d.ts
@@ -9,6 +9,7 @@ import * as proto_services_report_report_service_pb from "../../../proto/service
 
 interface IReportServiceService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
     createReport: IReportServiceService_ICreateReport;
+    listAllReports: IReportServiceService_IListAllReports;
 }
 
 interface IReportServiceService_ICreateReport extends grpc.MethodDefinition<proto_services_report_report_service_pb.CreateReportRequest, proto_services_report_report_service_pb.CreateReportResponse> {
@@ -20,17 +21,30 @@ interface IReportServiceService_ICreateReport extends grpc.MethodDefinition<prot
     responseSerialize: grpc.serialize<proto_services_report_report_service_pb.CreateReportResponse>;
     responseDeserialize: grpc.deserialize<proto_services_report_report_service_pb.CreateReportResponse>;
 }
+interface IReportServiceService_IListAllReports extends grpc.MethodDefinition<proto_services_report_report_service_pb.ListAllReportsRequest, proto_services_report_report_service_pb.ListAllReportsResponse> {
+    path: "/services.report.ReportService/ListAllReports";
+    requestStream: false;
+    responseStream: false;
+    requestSerialize: grpc.serialize<proto_services_report_report_service_pb.ListAllReportsRequest>;
+    requestDeserialize: grpc.deserialize<proto_services_report_report_service_pb.ListAllReportsRequest>;
+    responseSerialize: grpc.serialize<proto_services_report_report_service_pb.ListAllReportsResponse>;
+    responseDeserialize: grpc.deserialize<proto_services_report_report_service_pb.ListAllReportsResponse>;
+}
 
 export const ReportServiceService: IReportServiceService;
 
 export interface IReportServiceServer {
     createReport: grpc.handleUnaryCall<proto_services_report_report_service_pb.CreateReportRequest, proto_services_report_report_service_pb.CreateReportResponse>;
+    listAllReports: grpc.handleUnaryCall<proto_services_report_report_service_pb.ListAllReportsRequest, proto_services_report_report_service_pb.ListAllReportsResponse>;
 }
 
 export interface IReportServiceClient {
     createReport(request: proto_services_report_report_service_pb.CreateReportRequest, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.CreateReportResponse) => void): grpc.ClientUnaryCall;
     createReport(request: proto_services_report_report_service_pb.CreateReportRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.CreateReportResponse) => void): grpc.ClientUnaryCall;
     createReport(request: proto_services_report_report_service_pb.CreateReportRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.CreateReportResponse) => void): grpc.ClientUnaryCall;
+    listAllReports(request: proto_services_report_report_service_pb.ListAllReportsRequest, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.ListAllReportsResponse) => void): grpc.ClientUnaryCall;
+    listAllReports(request: proto_services_report_report_service_pb.ListAllReportsRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.ListAllReportsResponse) => void): grpc.ClientUnaryCall;
+    listAllReports(request: proto_services_report_report_service_pb.ListAllReportsRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.ListAllReportsResponse) => void): grpc.ClientUnaryCall;
 }
 
 export class ReportServiceClient extends grpc.Client implements IReportServiceClient {
@@ -38,4 +52,7 @@ export class ReportServiceClient extends grpc.Client implements IReportServiceCl
     public createReport(request: proto_services_report_report_service_pb.CreateReportRequest, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.CreateReportResponse) => void): grpc.ClientUnaryCall;
     public createReport(request: proto_services_report_report_service_pb.CreateReportRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.CreateReportResponse) => void): grpc.ClientUnaryCall;
     public createReport(request: proto_services_report_report_service_pb.CreateReportRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.CreateReportResponse) => void): grpc.ClientUnaryCall;
+    public listAllReports(request: proto_services_report_report_service_pb.ListAllReportsRequest, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.ListAllReportsResponse) => void): grpc.ClientUnaryCall;
+    public listAllReports(request: proto_services_report_report_service_pb.ListAllReportsRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.ListAllReportsResponse) => void): grpc.ClientUnaryCall;
+    public listAllReports(request: proto_services_report_report_service_pb.ListAllReportsRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: proto_services_report_report_service_pb.ListAllReportsResponse) => void): grpc.ClientUnaryCall;
 }
--- services/reportService/src/index.ts
+++ services/reportService/src/index.ts
@@ -5,11 +5,14 @@ import {
   ServerCredentials,
 } from "@grpc/grpc-js";
 import { dataSource } from "./app-data-source";
-import { Report } from "./data/entities/report.entity";
+import { Report as ReportEntity } from "./data/entities/report.entity";
 import { ReportServiceService } from "./grpc/proto/services/report/report_service_grpc_pb";
 import {
   CreateReportRequest,
   CreateReportResponse,
+  ListAllReportsRequest,
+  ListAllReportsResponse,
+  Report,
 } from "./grpc/proto/services/report/report_service_pb";
 
 import dotenv from "dotenv-safe";
@@ -20,11 +23,10 @@ dotenv.config({
 
 dataSource.initialize().then(() => console.log("Datasource initialized"));
 
-const repository = dataSource.getRepository(Report);
+const repository = dataSource.getRepository(ReportEntity);
 
-const convertToEntity = (proto: CreateReportRequest): Report => {
-
-  const report = new Report();
+const convertToEntity = (proto: CreateReportRequest): ReportEntity => {
+  const report = new ReportEntity();
   report.anonymous = proto.getReport()?.getAnonymous() ?? false;
   report.description = proto.getReport()?.getDescription() ?? "";
   report.latitude = proto.getReport()?.getLatitude() ?? 0;
@@ -53,38 +55,63 @@ const createReport = (
   }
 };
 
-var amqp = require('amqplib/callback_api');
-
-amqp.connect('amqp://localhost', function(error0: any, connection: any) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function(error1: any, channel: any) {
-        if (error1) {
-            throw error1;
-        }
-
-        var queue = 'report';
-
-        channel.assertQueue(queue, {
-            durable: false
-        });
-
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-
-        channel.consume(queue, function(msg: any) {
-            const newReport = msg.content;
-            console.log(`${newReport}`);
-            repository.save(newReport);
-        }, {
-            noAck: true
-        });
+const listAllReports = async (
+  call: ServerUnaryCall<ListAllReportsRequest, ListAllReportsResponse>,
+  callback: sendUnaryData<ListAllReportsResponse>
+) => {
+  try {
+    const response = await repository.find().then((reports) => {
+      return reports.map((report) =>
+        new Report()
+          .setAnonymous(report.anonymous)
+          .setDescription(report.description)
+          .setLatitude(report.latitude)
+          .setLongitude(report.longitude)
+          .setReportDate(report.report_date)
+          .setType(report.type)
+      );
     });
+    callback(null, new ListAllReportsResponse().setReportsList(response));
+  } catch (e) {}
+};
+
+var amqp = require("amqplib/callback_api");
+
+amqp.connect("amqp://localhost", function (error0: any, connection: any) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1: any, channel: any) {
+    if (error1) {
+      throw error1;
+    }
+
+    var queue = "report";
+
+    channel.assertQueue(queue, {
+      durable: false,
+    });
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+    channel.consume(
+      queue,
+      function (msg: any) {
+        const newReport = msg.content;
+        console.log(`AQUII`);
+        console.log(`${Object.values(newReport)}`);
+        repository.save(newReport);
+      },
+      {
+        noAck: true,
+      }
+    );
+  });
 });
 
 const server = new Server();
 
-server.addService(ReportServiceService, { createReport });
+server.addService(ReportServiceService, { createReport, listAllReports });
 
 server.bindAsync("0.0.0.0:4000", ServerCredentials.createInsecure(), () => {
   server.start();
