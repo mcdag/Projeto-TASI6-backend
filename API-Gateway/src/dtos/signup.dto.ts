90194cc539d3c1924554cf2731bbdb068cff146a
Felipe Araujo <fbma@cin.ufpe.br>
2023-04-30 14:49:31 -0300

    Add signup endpoint to api-gateway

---
+++ API-Gateway/src/dtos/signup.dto.ts
@@ -0,0 +1,24 @@
+import {
+  SignUpRequest,
+  User,
+} from "../grpc/proto/services/user/user_service_pb";
+
+export class SignUpRequestDTO {
+  name!: string;
+  password!: string;
+  email!: string;
+
+  constructor(dto: Partial<SignUpRequestDTO>) {
+    Object.assign(this, dto);
+  }
+
+  public toProto(): SignUpRequest {
+    const proto = new SignUpRequest();
+    const user = new User()
+      .setEmail(this.email)
+      .setName(this.name)
+      .setPassword(this.password);
+    console.log(`Generated proto: ${proto.getUser()?.getName}`);
+    return proto;
+  }
+}
